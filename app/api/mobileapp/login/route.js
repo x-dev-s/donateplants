b66189aeebe768fa7520a65d8053fc814a6c6f55
x-dev-s <sultan4412472@cloud.neduet.edu.pk>
2024-05-13 07:23:52 +0500

    commit

---
+++ app/api/mobileapp/login/route.js
@@ -0,0 +1,34 @@
+import User from "@/models/user"
+import connect from "@/utils/db"
+import bcrypt from "bcryptjs"
+import { NextResponse } from 'next/server';
+import { login } from '@/lib';
+import { cookies } from "next/headers";
+
+export async function POST(request) {
+  const credentials = await request.json();
+  await connect();
+  try {
+    const user = await User.findOne({ email: credentials.email });
+    if (user) {
+      const isPasswordCorrect = await bcrypt.compare(
+        credentials.password,
+        user.password
+      );
+      if (isPasswordCorrect) {
+        const data = await login({ email: credentials.email, name: user.name.replace(/\s/g, ''), id: user._id });
+        return NextResponse.json(data, { status: 200 });
+      }
+      else {
+        return new NextResponse("Password is incorrect", { status: 400 });
+      }
+    }
+    else {
+      return new NextResponse("This email is not registered", { status: 400 });
+    }
+  } catch (err) {
+    console.error(err);
+    throw new Error(err);
+  }
+
+}
