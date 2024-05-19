d644ee80d52b6e84f8a8281050f3e19222874a45
x-dev-s <sultan4412472@cloud.neduet.edu.pk>
2024-05-19 08:09:56 +0500

    commit

--- app/api/mobileapp/currencyexchange/route.js
+++ app/api/mobileapp/currencyexchange/route.js
@@ -4,7 +4,6 @@ import { NextResponse } from "next/server";
 export async function POST(request) {
     try {
         const { from, to } = await request.json();
-        
         return NextResponse.json({ rate: await GetCurencyExchangeRate(from, to) }, { status: 200 });
     }
     catch (err) {
